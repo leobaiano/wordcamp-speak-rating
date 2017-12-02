efafe3cd3f82312c09c822bda4a2f28372289c5a
Thauan Santos <nerooficial@gmail.com>
2017-12-02 16:35:57 -0200

    Estrutura do plugin definida

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WordCamp Speak Rating
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Plugin para avaliação de pública das submissões de palestras dos WordCamps.
+Version:      1.0.0
+Author:       WordCamp
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wc-speak-rating
+Domain Path:  /languages
+*/
